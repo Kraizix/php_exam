20350d2fa23cd1473664ebeed472f4df606ab468
VDAUTREMENT <valentin.dautrement@ynov.com>
2021-12-21 12:16:20 +0100

    add first styles home

--- public/home.php
+++ public/home.php
@@ -52,35 +52,38 @@ $results->execute();
 $posts=$results->fetchAll();
 foreach ($posts as $post){
     ?>
-            <h3><?= $post["title"]?></h3>
-            <h4><?php
-            $catArray = unserialize($post["category"]);
-            foreach ($catArray as $category) {
-                echo $category . " ";
-            }
-        ?></h4>
-            <p><?= $post["content"]?></p>
-            <h4>By <?= $post["userID"]?> <i> Ajouter le join pour le username plus tard !</i></h4>
-            <h4><?= $post["pinned"] == 1 ? "Pinned" : "Not Pinned" ?></h4>
-            <p>-----------------------</p>
-            <?php
-}
-?>
-
+            <div class="ui raised link card">
+                <div class="content">
+                    <div class="header"><?= $post['title'] ?></div>
+                    <div class="meta">
+                        <?php
+                        $catArray = unserialize($post["category"]);
+                        foreach ($catArray as $category) { ?>
+                        <div class="ui label"><?= $category ?></div>
+                        <?php } ?>
+                    </div>
+                    <div class="description">
+                        <p><?= $post["content"]?></p>
+                    </div>
+                </div>
+                <div class="extra content">
+                    By User_<?= $post["userID"] ?> -- <?= $post["pinned"] == 1 ? "Pinned" : "Not Pinned" ?>
+                </div>
+            </div>
+            <?php } ?>
             <br>
             <h2>Tendances :</h2>
 
             <?php
-//Tendances :
+            //Tendances :
+                $favQuery = "SELECT Articles.id, title, content, image, category, date, Articles.userID, pinned, count(Favs.id) AS nbFavs
+                FROM Articles INNER JOIN Favs ON Articles.id = postID GROUP BY postID ORDER BY count(Favs.id) DESC LIMIT 4";
+                $results = $pdo->prepare($favQuery);
+                $results->execute();
+                $posts = $results->fetchAll();
 
-$favQuery = "SELECT Articles.id, title, content, image, category, date, Articles.userID, pinned, count(Favs.id) AS nbFavs
-FROM Articles INNER JOIN Favs ON Articles.id = postID GROUP BY postID ORDER BY count(Favs.id) DESC LIMIT 4";
-$results = $pdo->prepare($favQuery);
-$results->execute();
-$posts = $results->fetchAll();
-
-foreach ($posts as $post){
-    ?>
+                foreach ($posts as $post){
+            ?>
             <h3><?= $post["title"]?></h3>
             <h4><?php
             $catArray = unserialize($post["category"]);
@@ -103,10 +106,8 @@ foreach ($posts as $post){
                 </div>
                 <div>
                     <!--Dropdown style VALENTIN -->
-
                     <label for="category">Category:</label>
-                    <select name="category" id="category">
-                        <option value="none">--Please choose an category--</option>
+                    <select name="category[]" class="ui selection dropdown" multiple="" id="multi-select">
                         <option value="informatique">Informatique</option>
                         <option value="new">New</option>
                         <option value="anime">Animé</option>
@@ -118,9 +119,10 @@ foreach ($posts as $post){
 
             <?php
         if (isset($_GET['q']) && $_GET['q'] != "") {
-            if ($_GET['category'] != "none") {
+            if ($_GET['category'] != "") {
                 $squery = "SELECT * FROM Articles WHERE (category = :category AND title LIKE :query) OR (category = :category AND content LIKE :query)";
-                $catArray = explode(',', $_GET['category']);
+                print_r($_GET['category']);
+                $catArray = $_GET['category'];
                 $category = serialize($catArray);
 
                 $datas = [
@@ -162,8 +164,15 @@ foreach ($posts as $post){
         ?>
     </div>
     <script>
+        $(document).ready(function () {
+            $('#multi-select')
+                .dropdown();
+        })
+
         $('button').click(function () {
-            $('.ui.sidebar').sidebar('toggle');
+            $('.ui.sidebar')
+                .sidebar('setting', 'transition', 'overlay')
+                .sidebar('toggle')
         })
     </script>
 </body>
